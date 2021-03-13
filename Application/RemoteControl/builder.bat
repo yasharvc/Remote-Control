73c1a625712f5aeba4a7621c482bf15279a81db6
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-13 20:24:25 +0330

    Don't wait for program ;)

--- Application/RemoteControl/builder.bat
+++ Application/RemoteControl/builder.bat
@@ -15,5 +15,5 @@ rmdir "D:\YasharRemote\Remote\app" /s /q
 cd "D:\YasharRemote\Projects\Remote-Control"
 dotnet publish -c Release --self-contained --runtime win-x86 -o "D:\YasharRemote\Remote\app"
 
-D:\YasharRemote\Remote\app\RemoteControl.exe
+start "" "D:\YasharRemote\Remote\app\RemoteControl.exe"
 DEL "%~f0"

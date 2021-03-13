4611873ddb7561672d62abc27b868a811bd8b2d3
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-13 21:06:19 +0330

    Updated some visual

--- Application/RemoteControl/MainForm.Designer.cs
+++ Application/RemoteControl/MainForm.Designer.cs
@@ -29,6 +29,7 @@ namespace RemoteControl
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.TakeScreenShootButton = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.RebuildButton = new System.Windows.Forms.Button();
@@ -49,10 +50,11 @@ namespace RemoteControl
 			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
 			this.textBox1.Location = new System.Drawing.Point(12, 41);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(776, 397);
+			this.textBox1.Size = new System.Drawing.Size(298, 158);
 			this.textBox1.TabIndex = 3;
 			// 
 			// RebuildButton
@@ -69,11 +71,12 @@ namespace RemoteControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(322, 211);
 			this.Controls.Add(this.RebuildButton);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.TakeScreenShootButton);
 			this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Main Form";
